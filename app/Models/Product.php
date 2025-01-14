5edfae95efde167c1537f0b6d4f11ed8c054e7e9
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-01-14 02:13:58 +0100

    Add MoneyPHP for monetary value handling and formatting
    Integrated MoneyPHP library to handle monetary values, including converting product prices into Money objects. Updated Blade templates to use a custom formatter for Money instances, ensuring consistent currency formatting across views. Enhanced the `Product` model with a `price` accessor for Money object handling.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,15 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Money\Currency;
+use Money\Money;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    public function price():Attribute
+    {
+        return Attribute::make(
+            get: function (int $value) {
+                return new Money($value, new Currency('USD'));
+        });
+    }
 
     public function variants(): HasMany
     {
--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,6 +1,6 @@
 <div class="grid grid-cols-4 gap-4">
     @foreach ($this->products as $product)
-        <div>
+        <div class="bg-white rounded-lg shadow p-4">
             <img src="{{ $product->image->path }}" alt="">
             <h2 class="font-medium text-lg">{{ $product->name }}</h2>
             <span class="text-gray-700 text-sm"> {{ $product->price }}</span>
