5f3bebcd5494e9efed3d8c8a96fc042330370740
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-01-19 02:12:18 +0100

    Switch to markdown table for order confirmation emails
    Replaced HTML table with a markdown table for better compatibility with email rendering across different clients. This improves readability and ensures consistent formatting of order details. Minor text adjustments were also made to streamline the email content.

--- resources/views/emails/order-confirmation.blade.php
+++ resources/views/emails/order-confirmation.blade.php
@@ -1,91 +1,27 @@
 @component('mail::message')
 
-    Order Confirmation
+    Hey {{ $order->user->name }},
 
-    Hi {{ $order->user->name }},
+    Thank you for your order. You can find all the details below.
 
-    Thank you for your order! We're excited to get it to you as soon as possible.
+    @component('mail::table')
+        | Item                         | Price              | Quantity           | Tax           | Total   |
+        | :--------------------------- | ------------------:| ------------------:| -------------:| -------:|
+        @foreach($order->items as $item)
+            | **{{ $item->name }}** <br>{{ $item->description }} | {{ $item->price }} | {{ $item->quantity }} | {{ $item->amount_tax }} | {{ $item->amount_total }} |
+        @endforeach
+        @if($order->amount_shipping->isPositive())
+            |||| **Shipping** | {{ $order->amount_shipping }} |
+        @endif
+        @if($order->amount_discount->isPositive())
+            |||| **Discount** | {{ $order->amount_discount }} |
+        @endif
+        @if($order->amount_tax->isPositive())
+            |||| **Tax** | {{ $order->amount_tax }} |
+        @endif
+        |||| **Subtotal** | {{ $order->amount_subtotal }} |
+        |||| **Total** | {{ $order->amount_total }} |
+    @endcomponent
 
-<table>
-    <thead>
-        <tr>
-            <th>Item</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Tax</th>
-            <th>Total</th>
-        </tr>
-    </thead>
-    <tbody>
-
-    @foreach ($order->items as $item)
-        <tr>
-            <td>{{ $item->name }} <br> {{ $item->description }}</td>
-            <td>{{ $item->price }}</td>
-            <td>{{ $item->quantity }}</td>
-            <td>{{ $item->amount_tax }}</td>
-            <td>{{ $item->amount_total }}</td>
-        </tr>
-    @endforeach
-
-    </tbody>
-    <tfoot>
-    @if($order->amount_shipping->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                Shipping Costs
-            </td>
-            <td colspan="4" style="text-align: right;">
-                {{ $order->amount_shipping }}
-            </td>
-        </tr>
-    @endif
-
-    @if($order->amount_discount->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                Discount
-            </td>
-            <td colspan="4" style="text-align: right;">
-                {{ $order->amount_discount }}
-            </td>
-        </tr>
-    @endif
-
-    @if($order->amount_tax->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                Tax
-            </td>
-            <td colspan="4" style="text-align: right;">
-                {{ $order->amount_tax }}
-            </td>
-        </tr>
-    @endif
-
-    @if($order->amount_subtotal->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                SubTotal
-            </td>
-            <td colspan="4" style="text-align: right;">
-                {{ $order->amount_subtotal }}
-            </td>
-        </tr>
-    @endif
-
-    @if($order->amount_total->isPositive())
-        <tr>
-            <td colspan="4" style="font-weight: bold; text-align: right;">
-                Total:
-            </td>
-            <td colspan="4" style="font-weight: bold; text-align: right;">
-                {{ $order->amount_total }}
-            </td>
-        </tr>
-    @endif
-    </tfoot>
-
-</table>
 
 @endcomponent
